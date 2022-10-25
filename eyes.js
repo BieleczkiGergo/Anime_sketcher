59fa10c26ad9faa1d3d88facfb7645c2731d93a0
Tipikaly <bgergo8b@gmail.com>
2022-10-25 20:59:33 +0200

    I tried...

--- eyes.js
+++ eyes.js
@@ -13,14 +13,22 @@ function setup(){
 }
 
 function drawTest(){
-    drawEyeFrame(20, 40);
+    drawEyeFrame(20, 40, 80, 10, 0.6, 2);
 
 }
 
-function drawEyeFrame(sx, sy, w, h){
+function drawEyeFrame(sx, sy, w, h, slope, upCurve){
+    //Starting x & y position, width, height, eye slope
     ctx.beginPath();
     ctx.moveTo(sx, sy);
-    ctx.bezierCurveTo(0, 0, 0, 0, w, h);
+    ctx.bezierCurveTo(
+        sx+ w*(1/5),        //control point 1 x
+        sy - h*upCurve,     //control point 1 y
+        sx + w*(4/10)*slope,       //control point 2 x
+        sy - h*upCurve*2,     //control point 2 y
+        sx+w,               //end point x
+        sy - slope*h        //end point y
+    );
 
 
     ctx.closePath();
